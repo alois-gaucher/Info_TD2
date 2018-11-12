3bb647e8aa5d7adfa3849858d3eb530dec644827
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2018-11-12 10:44:48 +0100

    Fonctions absolu (float et int), minimum (float et int)
    La fonction minimum float ne fonctionne pas encore tout à fait

---
+++ Exercice-1.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+                            TD Informatique
+							 Aloïs GAUCHER
+*******************************************************************************/
+
+#include <stdio.h>
+
+int valint, aint, bint, minimumint;
+float valfloat, afloat, bfloat, minimumfloat;
+
+//Fonction valeur absolue
+int absolueint (int valint) {
+	if (valint < 0) {
+		return -valint;
+	}
+	else {
+		return valint;
+	}
+}
+
+//Fonction valeur absolue flottant
+float absoluefloat (float valfloat) {
+	if (valfloat < 0) {
+		return -valfloat;
+	}
+	else {
+		return valfloat;
+	}
+}
+
+//Fonction minimum entier
+int miniint (int aint,int bint) {
+	if (aint < bint) {
+		minimumint = aint;
+	}
+	else if (aint > bint) {
+		minimumint = bint;
+	}
+	return minimumint;
+}
+
+//Fonction minimum float
+float minifloat (float afloat,float bfloat) {
+	if (afloat < bfloat) {
+		minimumfloat = afloat;
+	}
+	else if (afloat > bfloat) {
+		minimumfloat = bfloat;
+	}
+	return minimumfloat;
+}
+
+//Fonction discriminant
+
+
+//Fonction main
+int main() {
+	printf("Veuillez entrer un float: \n");
+	scanf("%f", &afloat);
+	printf("Veuillez entrer un autre float: \n");
+	scanf("%f", &bfloat);
+	minimumfloat = minifloat(aint, bint);
+	printf("La valeur minimale de ces float est: %f \n", minimumfloat);
+}
