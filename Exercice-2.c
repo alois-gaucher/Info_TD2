c8ecb2b23008da31381d46f3f5074f4481766ca9
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2018-11-19 09:51:07 +0100

    Terminé jusqu'à l'Exo 2 question 2

--- Exercice-2.c
+++ Exercice-2.c
@@ -8,7 +8,7 @@
 
 int n,selection;
 
-//Tous les entiers entre zéro et N
+//Tous les entiers entre zéro et n
 void tousentiers(int n) {
 int i;
 for (i = 0 ; i <= n ; i++) 
@@ -17,20 +17,56 @@ for (i = 0 ; i <= n ; i++)
 	}
 }
 
+//Affiche tous les nombres pairs entre zéro et n
+void touspairs(int n) {
+int i;
+for (i=1; i<=n; i++) {
+		if (i%2 == 0) {
+			printf("%d \n",i);
+		}		
+	} 
+}
+
+//Affiche tous les nombres impairs entre zéro et n
+void tousimpairs(int n) {
+int i;
+for (i=1; i<=n; i++) {
+		if (i%2 != 0) {
+			printf("%d \n",i);
+		}		
+	} 
+}
+
 int main() {
 
 	//Menu
 	printf("Veuillez choisir l'opération à réaliser: \n");
 	printf("1 - Affichage de tous les entiers entre 0 et n\n");
+	printf("2 - Affichage de tous les entiers pairs entre 0 et n\n");
+	printf("3 - Affichage de tous les entiers impairs entre 0 et n\n");
 	scanf("%d" ,&selection);
 
 		switch(selection)
 		{
-			case 1:
+			case 1: //Affiche tous les entiers de 0 à n
 			printf("Veuillez entrer un entier n: \n");
 			scanf("%d", &n);
 			printf("Tous les entiers compris entre 0 et %d: \n",n);
 			tousentiers(n);
+			break;
+
+			case 2: //Affiche tous les entiers pairs de 0 à n
+			printf("Veuillez entrer un entier n: \n");
+			scanf("%d", &n);
+			printf("Tous les entiers pairs compris entre 0 et %d: \n",n);
+			touspairs(n);
+			break;
+
+			case 3: //Affiche tous les entiers impairs de 0 à n
+			printf("Veuillez entrer un entier n: \n");
+			scanf("%d", &n);
+			printf("Tous les entiers impairs compris entre 0 et %d: \n",n);
+			tousimpairs(n);
 			break;
 	
 			default:
